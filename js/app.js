af16ec4c5194f656165810d87e5eeaba159e9847
emowtion <46008834+emowtion@users.noreply.github.com>
2020-06-22 11:35:18 +0200

    checkWin added

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 const $qwerty = $('#qwerty');
 const $phrase = $('#phrase');
 const $startBtn = $('.btn__reset');
-let guess = 0;
+let missed = 0;
 
 const phrases = ['i love you',  // create an array of phrases
     'i miss you',
-    'you aremy life',
+    'you are my life',
     'you are my breath',
     'you are my cat'
 ];
@@ -28,10 +28,10 @@ function addPhraseToDisplay(arr) {
     const liContent = document.createTextNode(arr[i]);
     listItems.appendChild(liContent);
     docFragment.appendChild(listItems);
-    if (listItems.textContent === arr[i]) {
-        listItems.classList.add('letter')
-    } else  {
+    if (listItems.textContent === " ") {
         listItems.classList.add('space')
+    } else  {
+        listItems.classList.add('letter')
     }
 }
     $('#phrase ul').append(docFragment);
@@ -41,15 +41,46 @@ addPhraseToDisplay(phraseArray);
 
 
 function checkLetter (button) {
-  
+   
+  let list = document.getElementsByClassName('letter');
+  let match = null;
+  for (let i = 0; i < list.length; i++) {
+      if (button === list[i].innerHTML) {
+          list[i].classList.add('show');
+          match = button;
+      } else {
+          match = null;
+      }
+  }
+  return match;
 };
 
 $startBtn.click(function () {
     $('#overlay').hide()
-
-    
 });
 
 $qwerty.on( "click", function( event ) {
-    checkLetter (event.button)
+   let chosen = event.target;
+   chosen.className += 'chosen';
+   
+    let letterFound = event.target.innerHTML;
+    checkLetter(letterFound);
+   /*  let heart = document.querySelectorAll('.tries')
+    if (letterFound === null) {
+        heart.nextElementSibling.remove()
+        missed++;
+    }  */
+    checkWin();
   });
+
+
+  function checkWin() {
+    let show = document.querySelectorAll('.show').length;
+    let letter = document.querySelectorAll('.letter').length;
+   
+    if (show === letter) {
+        document.querySelector('.win').style.display = 'block';
+    } if(missed => 5) {
+        document.querySelector('.lose').style.display = 'block';
+    }
+  }
